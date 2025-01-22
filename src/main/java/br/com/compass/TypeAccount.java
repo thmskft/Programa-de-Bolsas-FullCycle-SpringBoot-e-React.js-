32e63f5b182637d7fcd9a2ad8a64ad32fd6912d6
thmskft <martinsdemoura94@gmail.com>
2025-01-22 20:15:09 -0300

    add toString

--- src/main/java/br/com/compass/TypeAccount.java
+++ src/main/java/br/com/compass/TypeAccount.java
@@ -4,4 +4,13 @@ public enum TypeAccount {
     SAVINGS_ACCOUNT,
     CURRENT_ACCOUNT,
     SALARY_ACCOUNT;
+
+    @Override
+    public String toString() {
+        return switch (this) {
+            case SAVINGS_ACCOUNT -> "Savings Account";
+            case CURRENT_ACCOUNT -> "Current Account";
+            case SALARY_ACCOUNT -> "Salary Account";
+        };
+    }
 }
